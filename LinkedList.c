e7f49baa6177e0a887cd6cca8c8e040c75dfef4c
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-10-19 23:58:02 -0300

    something like an outline

--- LinkedList.c
+++ LinkedList.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+typedef struct _tipo_MemLivre
+{
+    int End_i;
+    int tam;
+    struct _tipo_MemLivre *prox;
+} tipo_MemLivre;
+
+typedef struct _tipo_MemAloc
+{
+    int NProcesso;
+    int End_i;
+    int tam;
+    struct _tipo_MemAloc *prox;
+} tipo_MemAloc;
+
+void inicia (void)
 {
 
-    return 0;
+}
+
+void insereBlocoAloc (int nProcesso, int end_i, int tam)
+{
+
+}
+
+void insereBlocoMemLivre (int end_i, int tam)
+{
+
+}
+
+int alocaMemoria (int nProcesso, int Tam)
+{
+
+}
+
+int buscaEspacoDisp (int Tam)
+{
+
+}
+
+void finalizProcesso (int n)
+{
+
+}
+
+void organizaBlocoMemLivre (void)
+{
+
+}
+
+void liberaLista (void)
+{
+    
 }
