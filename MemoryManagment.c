10a48b573d1368a66114d9f80b542212557b3725
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-10-23 23:20:48 -0300

    Update MemoryManagment.c

--- MemoryManagment.c
+++ MemoryManagment.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "LinkedList.c"
 
 int main()
 {
     int mem_disp;
 
-    inicia();
+    printf("Quantidade de memória disponível? ");
+    scanf("%d", &mem_disp);
+    while(true)
+    {
+        
+    }
+
+
+    return 0;
+}
+
+
+/*
     insereBlocoMemLivre(100, 50);
     insereBlocoMemLivre(150, 30);
     insereBlocoAloc(1, 200, 10);
     insereBlocoAloc(2, 210, 20);
-
-    /*
-    printf("Quantidade de memória disponível? ");
-    scanf("%d", &mem_disp);
-    */
-
     exibeMemLivre();
     exibeMemAloc();
-    return 0;
-}
+*/
