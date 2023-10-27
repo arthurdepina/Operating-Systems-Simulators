957d41ca27b16154e8f3918ba6b8301a65752f91
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-10-27 01:21:46 -0300

    teste alocaMemoria 1

--- teste.c
+++ teste.c
@@ -7,7 +7,9 @@ int main ()
     insereBlocoMemLivre(1800, 2200);
     insereBlocoMemLivre(2300, 400);
     exibeMemLivre();
-    printf("%d\n", buscaEspacoDisp(300));
-    printf("%d\n", minimo_bloco_suficiente(300));
+    printf("\nEncontrou espaço disponível: %d\n", buscaEspacoDisp(300));
+    printf("Enderço do Mínimo Bloco Suficiente: %d\n\n", minimo_bloco_suficiente(300));
+    alocaMemoria(1234, 300);
+    exibeMemLivre();
     return 0;
 }
