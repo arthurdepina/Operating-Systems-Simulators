bdc9b9daa24cb2dbdc5282b841193b3cf8b53a3b
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-11-26 03:48:55 -0300

    t2

---
+++ n-aryTree.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    char id[256];
+    char path[1024];
+    char tipo;
+    struct Node *dir_ant;
+    struct Node *prox;
+    struct None *filhos;
+} Node;
+
+Node* criarNo(const char* id, const char tipo, Node* dir_ant)
+{
+    Node* novoNo = (Node *) malloc (sizeof(Node));
+    if (novoNo) {
+        strcpy(novoNo->id, id);
+        novoNo->tipo = tipo;
+        novoNo->dir_ant = dir_ant;
+        novoNo->prox = NULL;
+        novoNo->filhos = NULL;
+    }
+}
---
+++ shell.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IDNAME 30;
+#define PATH 100;
